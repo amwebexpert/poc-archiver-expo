44f186d45cf5917f4db34cca1ce8387b1d084012
André Masson <amwebexpert@gmail.com>
2024-12-29 17:34:13 -0600

    feat: poc of sentiment analysis with Transformer.js

--- src/app/(drawer)/(home)/profile.tsx
+++ src/app/(drawer)/(home)/profile.tsx
@@ -1,13 +1,24 @@
 import { Text } from 'react-native';
 
-import { Container } from '~/components/safe-container';
+import { useEffect, useState } from 'react';
 import { FullCentered } from '~/components/full-centered';
+import { Container } from '~/components/safe-container';
+import { sentimentAnalysisAsString } from '~/services/text-classification';
 
 export default function Profile() {
+  const [textClassification, setTextClassification] = useState('');
+  const textToAnalyse = 'I love transformers!';
+  const textToAnalyse2 = 'I hate this food, not good at all!';
+
+  useEffect(() => {
+    sentimentAnalysisAsString(textToAnalyse2).then(setTextClassification);
+  }, []);
+
   return (
     <Container>
       <FullCentered>
         <Text>Profile Screen</Text>
+        <Text>{textClassification}</Text>
       </FullCentered>
     </Container>
   );
---
+++ src/utils/polyfills.ts
@@ -0,0 +1,18 @@
+import { Buffer } from 'buffer';
+import 'react-native-skia-offscreencanvas/polyfill';
+import 'text-encoding-polyfill'; // support TextEncoder
+import XRegExp from 'xregexp';
+
+global.Buffer = Buffer;
+
+// replace default RegExp to support Unicode
+const nativeRegExp = global.RegExp;
+const newRegExp = (...args: [string, string?]) => {
+  global.RegExp = nativeRegExp;
+  const result = XRegExp(...(args as [string, string?]));
+  global.RegExp = newRegExp as RegExpConstructor;
+  return result;
+};
+global.RegExp = newRegExp as RegExpConstructor;
+
+console.info('===> polyfills setup completed');
