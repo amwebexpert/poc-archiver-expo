402e1632ac72da85b7b9c18586d568aab23ec9b6
André Masson <amwebexpert@gmail.com>
2025-01-01 14:23:17 -0600

    feat: improve drop-down-list and bottom-sheet demos

--- src/app/(drawer)/other-experiments.tsx
+++ src/app/(drawer)/other-experiments.tsx
@@ -1,52 +1,21 @@
-import { BottomSheetModal, BottomSheetScrollView } from '@gorhom/bottom-sheet';
 import { Stack } from 'expo-router';
-import { FunctionComponent, useRef } from 'react';
+import { FunctionComponent } from 'react';
 import { StyleSheet } from 'react-native';
-import { Button } from 'react-native-paper';
-import WebView from 'react-native-webview';
-import { DefaultBackdrop } from '~/components/bottom-sheet/default-backdrop';
 
 import { SafeContainer } from '~/components/safe-container';
+import BottomSheetDemo from '~/features/experiments/bottom-sheet';
 import { DropDownList } from '~/features/experiments/drop-down-list';
 import { useAppTheme } from '~/theme/theme';
 
-const SNAP_POINTS = ['75%', '90%'];
-
 const DemoBottomSheet: FunctionComponent = () => {
   const styles = useStyles();
-
-  const bottomSheetModalRef = useRef<BottomSheetModal>(null);
-  const handlePresentModalPress = () => bottomSheetModalRef.current?.present();
-  const handleSheetChanges = (index: number) => console.info('handleSheetChanges', index);
 
   return (
     <SafeContainer style={styles.container}>
       <Stack.Screen options={{ headerTitle: 'Other POCs' }} />
 
-      <Button mode="contained" onPress={handlePresentModalPress}>
-        Show Modal
-      </Button>
-
+      <BottomSheetDemo />
       <DropDownList />
-
-      <BottomSheetModal
-        ref={bottomSheetModalRef}
-        index={0}
-        enablePanDownToClose={true}
-        backdropComponent={DefaultBackdrop}
-        enableDynamicSizing={false}
-        onChange={handleSheetChanges}
-        snapPoints={SNAP_POINTS}>
-        <BottomSheetScrollView style={styles.contentContainer}>
-          <WebView
-            style={{ height: 600 }}
-            source={{ uri: 'https://google.com' }}
-            nestedScrollEnabled={true}
-            javaScriptEnabled={true}
-            javaScriptCanOpenWindowsAutomatically={true}
-          />
-        </BottomSheetScrollView>
-      </BottomSheetModal>
     </SafeContainer>
   );
 };
@@ -58,10 +27,6 @@ const useStyles = () => {
     container: {
       margin: theme.spacing(3),
       gap: theme.spacing(3),
-    },
-    contentContainer: {
-      flex: 1,
-      width: '100%',
     },
   });
 };
