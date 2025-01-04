9a56aeb18145939b9cdfe8864b0877e0fa726ebb
André Masson <amwebexpert@gmail.com>
2025-01-04 14:34:27 -0600

    chore: expo update experiment with formatted local date string

--- src/features/about/about.details.tsx
+++ src/features/about/about.details.tsx
@@ -1,3 +1,4 @@
+import { format } from 'date-fns';
 import { FunctionComponent } from 'react';
 import { StyleSheet } from 'react-native';
 import { DataTable } from 'react-native-paper';
@@ -6,24 +7,26 @@ import { APP_VERSION_INFO } from '~/constants';
 
 const { AUTHOR, VERSION_DATE_ISO, NAME } = APP_VERSION_INFO;
 
-export const AboutDetails: FunctionComponent = () => (
-  <DataTable>
-    <DataTable.Row>
-      <DataTable.Cell>Name:</DataTable.Cell>
-      <DataTable.Cell style={styles.tableValueCell}>{NAME}</DataTable.Cell>
-    </DataTable.Row>
-    <DataTable.Row>
-      <DataTable.Cell>Author:</DataTable.Cell>
-      <DataTable.Cell style={styles.tableValueCell}>{AUTHOR}</DataTable.Cell>
-    </DataTable.Row>
-    <DataTable.Row>
-      <DataTable.Cell>Date ISO:</DataTable.Cell>
-      <DataTable.Cell style={styles.tableValueCell}>
-        {new Date(VERSION_DATE_ISO).toLocaleDateString()}
-      </DataTable.Cell>
-    </DataTable.Row>
-  </DataTable>
-);
+export const AboutDetails: FunctionComponent = () => {
+  const formattedDate = format(new Date(VERSION_DATE_ISO), 'yyyy-MM-dd HH:mm:ss');
+
+  return (
+    <DataTable>
+      <DataTable.Row>
+        <DataTable.Cell>Name:</DataTable.Cell>
+        <DataTable.Cell style={styles.tableValueCell}>{NAME}</DataTable.Cell>
+      </DataTable.Row>
+      <DataTable.Row>
+        <DataTable.Cell>Author:</DataTable.Cell>
+        <DataTable.Cell style={styles.tableValueCell}>{AUTHOR}</DataTable.Cell>
+      </DataTable.Row>
+      <DataTable.Row>
+        <DataTable.Cell>Date ISO:</DataTable.Cell>
+        <DataTable.Cell style={styles.tableValueCell}>{formattedDate}</DataTable.Cell>
+      </DataTable.Row>
+    </DataTable>
+  );
+};
 
 const styles = StyleSheet.create({
   tableValueCell: {
