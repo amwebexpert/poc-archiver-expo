91d57a873bf06b3ab3b33c1c8bf572699ab5d43e
André Masson <amwebexpert@gmail.com>
2025-07-12 16:30:32 -0400

    refactor: make app folder lean (only routing pointers)

--- app/(drawer)/other-experiments.tsx
+++ app/(drawer)/other-experiments.tsx
@@ -1,38 +1,5 @@
-import { Stack } from 'expo-router';
-import { FunctionComponent } from 'react';
-import { StyleSheet } from 'react-native';
+import OtherExperimentsScreen from '~/features/experiments/other-experiments-screen';
 
-import { SafeContainer } from '~/components/layout/safe-container';
-import ApiRoutesDemo from '~/features/experiments/api-routes';
-import BottomSheetDemo from '~/features/experiments/bottom-sheet';
-import { DropDownList } from '~/features/experiments/drop-down-list';
-import ToasterDemoScreen from '~/features/experiments/sunner-native';
-import { useAppTheme } from '~/theme/theme';
-
-const DemoBottomSheet: FunctionComponent = () => {
-  const styles = useStyles();
-
-  return (
-    <SafeContainer style={styles.container}>
-      <Stack.Screen options={{ headerTitle: 'Other POCs..' }} />
-
-      <ApiRoutesDemo />
-      <BottomSheetDemo />
-      <ToasterDemoScreen />
-      <DropDownList />
-    </SafeContainer>
-  );
-};
-
-const useStyles = () => {
-  const theme = useAppTheme();
-
-  return StyleSheet.create({
-    container: {
-      margin: theme.spacing(3),
-      gap: theme.spacing(2),
-    },
-  });
-};
-
-export default DemoBottomSheet;
+export default function OtherExperiments() {
+  return <OtherExperimentsScreen />;
+}
